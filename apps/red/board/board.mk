0906dd3008e1a44e2654c8142794872a052d3a54
Sergey Lapin <slapin@ossfans.org>
2013-02-18 18:24:09 +0400

    Updated stock board skeleton

--- apps/red/board/board.mk
+++
@@ -1,6 +0,0 @@
-# Required include directories
-BOARDINC = ./board
-
-# List of all the board related files.
-BOARDSRC = ${BOARDINC}/board.c
-
--- boards/ITETRA_K2/board.mk
+++ boards/ITETRA_K2/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = ${CHIBIOS}/boards/OLIMEX_SAM7_EX256/board.c
+BOARDSRC = ${CHIBIOS}/boards/ITETRA_K2/board.c
 
 # Required include directories
-BOARDINC = ${CHIBIOS}/boards/OLIMEX_SAM7_EX256
+BOARDINC = ${CHIBIOS}/boards/ITETRA_K2
