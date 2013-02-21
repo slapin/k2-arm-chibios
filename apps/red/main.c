d0f128100f21a5691f33257e0026d8e0db809c3e
Sergey Lapin <slapin@ossfans.org>
2013-02-21 04:53:17 +0400

    test dump of FRAM

--- apps/red/main.c
+++ apps/red/main.c
@@ -31,7 +31,7 @@ static EepromFileStream fram;
 #define EEPROM_PAGE_SIZE        128         /* page size in bytes. Consult datasheet. */
 #define EEPROM_SIZE             8192       /* total amount of memory in bytes */
 #define EEPROM_I2CD             I2CD1       /* ChibiOS I2C driver used to communicate with EEPROM */
-#define EEPROM_I2C_ADDR         0b1010000   /* EEPROM address on bus */
+#define EEPROM_I2C_ADDR         0x50   /* EEPROM address on bus */
 #define EEPROM_WRITE_TIME_MS    20          /* time to write one page in mS. Consult datasheet! */
 #define EEPROM_TX_DEPTH         (EEPROM_PAGE_SIZE + 2)/* temporal transmit buffer depth for eeprom driver */
 
@@ -67,6 +67,7 @@ static msg_t gnss(void *p) {
     chRegSetThreadName("gnss");
     while (TRUE) {
         chThdSleepMilliseconds(100);
+        chprintf((BaseSequentialStream*)&SD1, "");
 	    int t = sdGet(&SD1);
             chprintf((BaseSequentialStream*)&SDDBG, "z %d\r\n", t);
 
@@ -83,9 +84,9 @@ static msg_t gnss(void *p) {
 }
 
 static uint8_t frambuf[1024];
-#define I2C_CLDIV 10
-#define I2C_CHDIV 10
-#define I2C_CKDIV 4
+#define I2C_CLDIV 12
+#define I2C_CHDIV 12
+#define I2C_CKDIV 5
 
 static const I2CConfig i2cfg = {
 	.cwgr = (I2C_CKDIV << 16) | (I2C_CHDIV << 8) | (I2C_CLDIV)
@@ -95,6 +96,9 @@ static const I2CConfig i2cfg = {
  */
 int main(void) {
    int status;
+   uint8_t rxbuf[32];
+   uint8_t txbuf[16];
+   int j;
     /*
      * System initializations.
      * - HAL initialization, this also initializes the configured device drivers
@@ -107,6 +111,26 @@ int main(void) {
     k2_init_serials();
     
     i2cStart(&I2CD1, &i2cfg);
+    for (j = 0; j < 8192; j+= 16) {
+	    int k;
+	    memset(rxbuf, 0xff, sizeof(rxbuf));
+	    for (k = 0; k < 16; k++) {
+	        txbuf[0] = ((j + k) >> 8) & 0xff;
+	        txbuf[1] = ((j + k) & 0xff);
+	    	status = i2cMasterTransmitTimeout(&I2CD1, (0xa << 3), txbuf, 2, &rxbuf[k], 1, 2000);
+		if (status != RDY_OK)
+			break;
+	    }
+	    if (status == RDY_OK) {
+		    dbg_hex_dump(rxbuf, 16);
+	    }
+	    if (status == RDY_RESET)
+		    chprintf((BaseSequentialStream*)&SDDBG, "i2c reset\r\n");
+	    if (status == RDY_TIMEOUT)
+		    chprintf((BaseSequentialStream*)&SDDBG, "i2c timeout\r\n");
+    }
+    chprintf((BaseSequentialStream*)&SDDBG, "fram read status %d byte %02x\n", status, rxbuf[0]);
+#if 0
 
     EepromFileOpen(&fram, &icfg);
     chFileStreamSeek(&fram, 0);
@@ -116,7 +140,6 @@ int main(void) {
     else
             chprintf((BaseSequentialStream*)&SDDBG, "fram read failure %d\n", status);
     chFileStreamClose(&fram);
-#if 0
 #endif
 
     /*
